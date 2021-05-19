64d29004672af2023d57f7a50ef5dd2b6319ba91
muhammetnurt <muhammetnurtoplu@hotmail.com>
2021-05-19 16:20:29 +0300

    ekle

--- CPU_SICAKLIK.c
+++ CPU_SICAKLIK.c
@@ -1,3 +1,11 @@
+/*
+Bu çalışma için aşağıdaki sitelerden yararlanılmıştır.
+	https://man7.org/linux/man-pages/man3/usleep.3.html
+	http://www.digitalruh.com/wordpress/2018/07/09/raspberry-pi-ile-thread-kullanarak-tcp-server-uygulamasi/
+	https://www.bilgigunlugum.net/prog/cprog/c_stdkut
+
+
+*/
 #include <stdio.h>
 #include <string.h>
 #include <wiringPi.h>
@@ -6,26 +14,25 @@
 #include <unistd.h>
 
 char CPUtempfile[] = {"/sys/class/thermal/thermal_zone0/temp"};
-
+//RPi için CPU sıcaklık bilgisinin tutulduğu adres.
 
 int main (int argc, char *argv[])
 {
 	FILE *ftemp;
 	float Sicaklik_Deger;
 	char* Sicaklik_Okunan, Sicaklik_index;
-	FILE *fp;
 	char filebuffer[100];
 	
 	do{
-	if((fp=fopen(CPUtempfile,"r")) == NULL)
+	if((ftemp=fopen(CPUtempfile,"r")) == NULL)
 	{
 	printf("Dosya açma hatası!\n(%s) Bulunamadı ...\n",CPUtempfile);
 	exit(EXIT_FAILURE);
 	}	
-	fread(filebuffer, sizeof(filebuffer), 1, fp);
-	fclose(fp);
+	fread(filebuffer, sizeof(filebuffer), 1, ftemp);//dosyayı okur ve değerleri filebuffer değişkenine atılır.
+	fclose(ftemp);
 	Sicaklik_Deger=atoi(filebuffer);
-	Sicaklik_Deger=Sicaklik_Deger/1000;
+	Sicaklik_Deger=Sicaklik_Deger/1000;//dosyada okunan değerin anlaşılır bir hale gelmesi gerekmektedir.
 	
 	printf("%5.2f\n",Sicaklik_Deger);
 	usleep(500*1000);
